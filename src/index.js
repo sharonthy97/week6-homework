a3f777d2dd0bb3dd9fb80177c842b1291f4b74d9
sharonthy97 <151848834+sharonthy97@users.noreply.github.com>
2024-05-29 20:07:39 +0800

    fix search function bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#speed");
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   getForecast(response.data.city);
@@ -39,12 +41,12 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "48ab3c475703c305f36e50o053ta6dba";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
-function searchSubmit(event) {
+function handleSearch(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
 
@@ -63,7 +65,7 @@ function displayForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
+  days.forEach(function (day) {
     forecastHtml =
       forecastHtml +
       `
@@ -85,13 +87,6 @@ function displayForecast(response) {
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
+searchForm.addEventListener("submit", handleSearch);
 
 searchCity("Kota Kinabalu");
-getForecast("Kota Kinabalu");
-displayForecast();
